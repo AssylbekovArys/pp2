23b5ca8a863f7512b1d4cc182ca161921c89a2a0
arystan <arystan.asylbekov@bk.ru>
2023-01-17 17:20:21 +0600

    FIrst commit

---
+++ q.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    string s, s1;
+    cin >> s >> s1;
+    string s2 = "";
+    for(int i = 0; i < s1.size() / s.size(); i++) {
+        s2 += s;
+    }
+
+    if(s2 == s1) {
+        cout << "YES";
+    } else {
+        cout << "NO";
+    }
+}
