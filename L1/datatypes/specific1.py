57bca8ad3605afda5475036fec1e8eb9990701f4
arystan <arystan.asylbekov@bk.ru>
2023-01-24 17:01:14 +0600

    3 new folders were added

---
+++ L1/datatypes/specific1.py
@@ -0,0 +1,104 @@
+#str
+x = str("Hello World")
+
+print(x)
+
+print(type(x)) 
+
+#int
+x = int(20)
+
+print(x)
+
+print(type(x)) 
+
+#float
+x = float(20.5)
+
+print(x)
+
+print(type(x)) 
+
+#complex
+x = complex(1j)
+
+print(x)
+
+print(type(x))
+
+#list
+x = list(["apple", "banana", "cherry"])
+
+print(x)
+
+print(type(x))
+
+#tuple
+x = tuple(("apple", "banana", "cherry"))
+
+print(x)
+
+print(type(x))
+
+#range
+x = range(6)
+
+print(x)
+
+print(type(x))
+
+#dict
+x = dict({"name" : "John", "age" : 36})
+
+print(x)
+
+print(type(x))
+
+#set
+x = set({"apple", "banana", "cherry"})
+
+print(x)
+
+print(type(x))
+
+#frozenset
+x = frozenset({"apple", "banana", "cherry"})
+
+print(x)
+
+print(type(x))
+
+#bool
+x = bool(5)
+
+print(x)
+
+print(type(x))
+
+#bytes
+x = bytes(5)
+
+print(x)
+
+print(type(x))
+
+#bytearray
+x = bytearray(5)
+
+print(x)
+
+print(type(x))
+
+#memoryview
+x = memoryview(bytes(5))
+
+print(x)
+
+print(type(x))
+
+#Nonetype
+x = None
+
+print(x)
+
+print(type(x))
---
+++ L1/numbers/float2.py
@@ -0,0 +1,11 @@
+x = 35e3
+y = 12E4
+z = -87.7e100
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+print(x)
+print(y)
+print(z)
